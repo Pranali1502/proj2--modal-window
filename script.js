953b3f31ee952d151147019fa25d47c813ae5f1e
Pranali1502 <mailto:pani10295@gmail.com>
2024-04-06 14:07:46 +0530

    refactoring the code

--- script.js
+++ script.js
@@ -2,5 +2,28 @@
 
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector("close-modal");
+const btnCloseModal = document.querySelector(".close-modal");
 const btnsShowModal = document.querySelectorAll(".show-modal");
+// console.log(btnsShowModal);
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+for (let i = 0; i < btnsShowModal.length; i++)
+  btnsShowModal[i].addEventListener("click", openModal);
+
+const closedModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+btnCloseModal.addEventListener("click", closedModal);
+
+overlay.addEventListener("click", closedModal);
+
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closedModal();
+  }
+});
